5d554d025eb84daa9902b27e12fc437620ca62c7
Aresfro <tatyanapronskih2016@gmail.com>
2024-05-14 01:38:46 +0300

    Типо коммит

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mainRoute = require('./routes/main');
+const gamesRouter = require('./routes/games'); 
+     
+const PORT = 3000;
+const app = express();
+     
+app.use(
+    cors,
+    bodyParser.json(),
+    mainRoute, 
+    gamesRouter, 
+    express.static(path.join(__dirname, 'public')),
+);
+ 
+     
+app.listen(PORT, () => {
+    console.log(`Server is running at PORT http://localhost:${PORT}`);
+})
