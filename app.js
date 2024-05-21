32dc94cbdc8cea5ea86ad2e75abb33bb7f128978
Aresfro <tatyanapronskih2016@gmail.com>
2024-05-21 11:01:43 +0300

    upd3:(

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cors = require('./middlewares/cors')
 const mainRoute = require('./routes/main');
 const gamesRouter = require('./routes/games'); 
 const PORT = 3000;
 const app = express();
 
 app.use(
-  path,
   cors,
   bodyParser.json(),
   mainRoute, 
